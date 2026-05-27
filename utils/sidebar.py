4329229ac6610c58ed96266eb37755cbd7c1440d
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-05-27 15:41:05 -0500

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -24,10 +24,10 @@ def render_sidebar():
     )
 
     workflow_items = [
-        ("Betting Board", "assets/icons/target.png"),
-        ("Line Movement / CLV", "assets/icons/chart.png"),
-        ("Model Lab", "assets/icons/flask.png"),
-        ("Data Maintenance", "assets/icons/database.png"),
+        ("Betting Board", "assets/icons/betting_board.png"),
+        ("Line Movement / CLV", "assets/icons/line_movement.png"),
+        ("Model Lab", "assets/icons/model_lab.png"),
+        ("Data Maintenance", "assets/icons/data_maintenance.png"),
     ]
 
     if "page" not in st.session_state:
