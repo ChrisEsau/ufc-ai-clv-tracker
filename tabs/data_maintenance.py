2b17e238718f35f840445c29c09a94605103f676
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-05-28 17:57:20 -0500

    Update data_maintenance.py

--- tabs/data_maintenance.py
+++ tabs/data_maintenance.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils.dm_workflow_controls import render_workflow_controls
 
 from utils.dm_validation_gate import (
     render_validation_gate,
@@ -20,5 +21,10 @@ def render_data_maintenance():
     )
 
     st.markdown("---")
+    
+    render_workflow_controls()
+    
+    st.markdown("---")
+
 
     render_validation_gate()
