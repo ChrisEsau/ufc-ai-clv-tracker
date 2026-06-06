c9cfd4a6da2a35d9bb606b6c633f5f52470a9f78
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-06-06 14:37:22 -0500

    Add prefight features module skeleton

---
+++ pipeline/features/base/prefight_features.py
@@ -0,0 +1,8 @@
+"""Prefight feature extraction for rolling UFC base features.
+
+Notebook source section:
+- PREFIGHT FEATURE FUNCTION
+
+Migration status:
+- Skeleton only. No notebook logic has been moved yet.
+"""
