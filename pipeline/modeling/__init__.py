08f81f1c7eaedef5cc01da440f8c5e898024223d
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-06-07 16:09:24 -0500

    Add modeling package initializer

---
+++ pipeline/modeling/__init__.py
@@ -0,0 +1,5 @@
+"""Modeling utilities for UFC prediction V2.
+
+This package contains model registry, config loading, artifact loading,
+prediction adapter, formatter, and algorithm prediction plug-ins.
+"""
