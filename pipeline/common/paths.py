59a2e79e21ff26f788d3dfccf5d1f4255e988847
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-06-11 23:23:56 -0500

    Add DraftKings event card match path

--- pipeline/common/paths.py
+++ pipeline/common/paths.py
@@ -89,6 +89,7 @@ MARKET_MATCH_AUDIT_V2_PATH = AUDITS_DIR / "ufc_market_match_audit_v2.parquet"
 DRAFTKINGS_MARKET_DIAGNOSTIC_PATH = MARKET_DIR / "draftkings_market_diagnostic.parquet"
 DRAFTKINGS_RAW_INDEX_PATH = MARKET_DIR / "draftkings_raw_index.parquet"
 DRAFTKINGS_EVENT_INDEX_PATH = MARKET_DIR / "draftkings_event_index.parquet"
+DRAFTKINGS_EVENT_CARD_MATCH_PATH = MARKET_DIR / "draftkings_event_card_matches.parquet"
 CANONICAL_MARKET_CATALOG_PATH = MARKET_DIR / "canonical_market_catalog.parquet"
 CANONICAL_MARKET_AUDIT_PATH = AUDITS_DIR / "canonical_market_catalog_audit.parquet"
 
