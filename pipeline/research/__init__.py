49e478224c98b7a8560ac24c6b4d12b1745140d7
ChrisEsau <48386935+ChrisEsau@users.noreply.github.com>
2026-06-14 23:22:08 -0500

    Add research package for standalone studies

---
+++ pipeline/research/__init__.py
@@ -0,0 +1,5 @@
+"""Standalone UFC research runners.
+
+These modules generate exploratory research artifacts and do not modify
+production master, feature, model, prediction, or market artifacts.
+"""
